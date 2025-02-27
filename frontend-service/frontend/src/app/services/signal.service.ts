8efe49ee0ed3cd7983dccac8a8d836300fe8d4cb
enesbuyukbas <enesbykbs20@gmail.com>
2025-02-27 03:22:02 +0300

    signal.service oluşturuldu

---
+++ frontend-service/frontend/src/app/services/signal.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalService {
+  private apiUrl = 'http://localhost:5000/api/signals'; // Backend API URL
+
+  constructor(private http: HttpClient) {}
+
+  getSignals(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}`);
+  }
+
+  getTopSignals(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/top`);
+  }
+}
